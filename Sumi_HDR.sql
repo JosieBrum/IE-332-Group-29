fa6515110feb5073daebac84dfd0ac3abf063c38
sumnima-bhandari <bhanda21@purdue.edu>
2025-11-21 11:26:34 -0500

    Create Sumi_HDR.sql

---
+++ Sumi_HDR.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.CompanyName,  
+    (COUNT(CASE WHEN ic.ImpactLevel = 'High' THEN 1 END) * 100 / NULLIF (COUNT(ic.EventID), 0))  
+    AS HDR 
+FROM ImpactsCompany ic 
+JOIN DisruptionEvent de 
+    ON ic.EventID = de.EventID 
+JOIN Company c 
+    ON ic.AffectedCompanyID = c.CompanyID
+WHERE
+    (c.CompanyName = 'Smith Inc')
+    AND (de.EventDate BETWEEN '2021-06-12' AND '2021-06-21')   
+GROUP BY 
+    c.CompanyName
