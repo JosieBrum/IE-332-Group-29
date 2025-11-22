012e7969e1dd7fbdfb96d88fd5b61ec766686df0
odutta36 <146301240+odutta36@users.noreply.github.com>
2025-11-21 21:22:25 -0500

    Update Joaquin_DF.sql

--- Joaquin_DF.sql
+++ Joaquin_DF.sql
@@ -1,4 +1,6 @@
 -- Disruption Frequency per company over a date range
+
+//Company 
 SELECT 
     c.CompanyName,
     COUNT(*) AS num_disruptions,
@@ -23,6 +25,30 @@ GROUP BY
 ORDER BY 
     df_per_month DESC;
 
+//Tier 
+SELECT 
+    c.CompanyName,
+    COUNT(*) AS num_disruptions,
+    (TIMESTAMPDIFF(MONTH, '2019-01-01', '2025-12-31') + 1) AS observation_period_months,
+    (COUNT(*) * 1.0 
+        / NULLIF(TIMESTAMPDIFF(MONTH, '2019-01-01', '2025-12-31') + 1, 0)
+    ) AS df_per_month
+FROM 
+    DisruptionEvent AS e
+    JOIN ImpactsCompany AS ic 
+        ON e.EventID = ic.EventID
+    JOIN Company AS c 
+        ON ic.AffectedCompanyID = c.CompanyID
+    JOIN Location AS l
+        ON c.LocationID = l.LocationID
+WHERE 
+    c.TierLevel = '1'
+    AND e.EventDate BETWEEN '2019-01-01' AND '2025-12-31'
+GROUP BY 
+    c.CompanyID,
+    c.CompanyName
+ORDER BY 
+    df_per_month DESC
 
 
 -- Network level 
@@ -45,4 +71,5 @@ WHERE
     --  Filters 
     AND ('1' = '' OR c.TierLevel = '1')
     AND ('' = '' OR l.CountryName = '')
+
     AND ('' = '' OR l.ContinentName = '');
