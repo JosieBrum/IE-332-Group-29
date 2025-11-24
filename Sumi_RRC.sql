0d060c8966b4adef0c85ce3fce40e33d7c56cbfe
sumnima-bhandari <bhanda21@purdue.edu>
2025-11-24 12:47:57 -0500

    Update Sumi_RRC.sql

--- Sumi_RRC.sql
+++ Sumi_RRC.sql
@@ -21,3 +21,29 @@ GROUP BY
     total.TotalEvents
 ORDER BY
     RRC DESC
+
+
+--RRC v2 (separated month and year, cant get all months to output, unsure about selecting continent or country)
+SELECT
+    CASE 
+        WHEN 'Continent' = 'Continent' THEN l.ContinentName
+        ELSE l.CountryName
+    END AS LocationName,
+    YEAR(de.EventDate) AS EventYear,
+    MONTH(de.EventDate) AS EventMonth,
+    COUNT(de.EventID) AS RRC_Count
+FROM DisruptionEvent de
+JOIN ImpactsCompany ic ON ic.EventID = de.EventID
+JOIN Company c ON ic.AffectedCompanyID = c.CompanyID
+JOIN Location l ON c.LocationID = l.LocationID
+WHERE
+    de.EventDate BETWEEN '2025-01-01' AND '2025-06-30'
+    AND ('' = '' OR c.TierLevel = '')
+GROUP BY
+    LocationName,
+    YEAR(de.EventDate),
+    MONTH(de.EventDate)
+ORDER BY
+    LocationName,
+    EventYear,
+    EventMonth;
