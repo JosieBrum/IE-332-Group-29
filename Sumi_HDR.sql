c61d4761091ea88e28f20c5c2d0acad045500b51
sumnima-bhandari <bhanda21@purdue.edu>
2025-12-03 12:11:52 -0500

    Update Sumi_HDR.sql

--- Sumi_HDR.sql
+++ Sumi_HDR.sql
@@ -1,9 +1,10 @@
--- Percent Contribution of Each Company Towards HDR
+-- Count and Percent Contribution of Each Company Towards HDR
 SELECT
     c.CompanyName, 
     l.ContinentName, 
     l.CountryName, 
     c.TierLevel,
+    COUNT(CASE WHEN ic.ImpactLevel = 'High' THEN 1 END) AS HighCount,
     (COUNT(CASE WHEN ic.ImpactLevel = 'High' THEN 1 END) * 100.0 / NULLIF((
             SELECT COUNT(*) 
             FROM ImpactsCompany ic
@@ -33,7 +34,7 @@ GROUP BY
     l.ContinentName, 
     l.CountryName, 
     c.TierLevel
-ORDER BY Percent DESC;
+ORDER BY HighCount DESC;
 
 -- Overall HDR Calculation (one value)
 SELECT
@@ -47,4 +48,29 @@ WHERE
     AND ('' = '' OR c.CompanyName = '')
     AND ('' = '' OR c.TierLevel = '')
     AND ('' = '' OR l.CountryName = '')
+    AND ('' = '' OR l.ContinentName = '');
+
+-- HDR by Company
+
+SELECT
+    c.CompanyName,
+    l.ContinentName,
+    l.CountryName,
+    c.TierLevel,
+    (COUNT(CASE WHEN ic.ImpactLevel = 'High' THEN 1 END) * 100.0 / NULLIF(COUNT(ic.EventID), 0)) AS CompanyHDR
+FROM ImpactsCompany ic
+JOIN DisruptionEvent de ON ic.EventID = de.EventID
+JOIN Company c ON ic.AffectedCompanyID = c.CompanyID
+JOIN Location l ON c.LocationID = l.LocationID
+WHERE
+    de.EventDate BETWEEN '2019-01-01' AND '2025-12-31'
+    AND ('' = '' OR c.CompanyName = '')
+    AND ('' = '' OR c.TierLevel = '')
+    AND ('' = '' OR l.CountryName = '')
     AND ('' = '' OR l.ContinentName = '')
+GROUP BY
+    c.CompanyName,
+    l.ContinentName,
+    l.CountryName,
+    c.TierLevel
+ORDER BY CompanyHDR DESC;
