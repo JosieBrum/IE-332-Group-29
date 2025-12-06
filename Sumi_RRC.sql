3d04a85e9225cafd6e9ac96668b8d1064b4e923d
odutta36 <146301240+odutta36@users.noreply.github.com>
2025-12-05 23:39:03 -0500

    Update Sumi_RRC.sql

--- Sumi_RRC.sql
+++ Sumi_RRC.sql
@@ -1,26 +1,47 @@
 -- RRC by Continent
 SELECT
-    l.ContinentName,
-    COUNT(*) * 100.0 / total.TotalEvents AS RRC
-FROM ImpactsCompany ic
-JOIN DisruptionEvent de ON ic.EventID = de.EventID
+    l.CountryName,
+    COUNT(DISTINCT de.EventID) * 100.0 / total.TotalEvents AS RRC
+FROM DisruptionEvent de
+JOIN ImpactsCompany ic ON ic.EventID = de.EventID
 JOIN Company c ON ic.AffectedCompanyID = c.CompanyID
 JOIN Location l ON c.LocationID = l.LocationID
 JOIN (
-    SELECT COUNT(*) AS TotalEvents
-    FROM ImpactsCompany ic
-    JOIN DisruptionEvent de ON ic.EventID = de.EventID
-    JOIN Company c ON ic.AffectedCompanyID = c.CompanyID
-    JOIN Location l ON c.LocationID = l.LocationID
-    WHERE de.EventDate BETWEEN '2021-01-01' AND '2021-12-31'
+    SELECT COUNT(DISTINCT de2.EventID) AS TotalEvents
+    FROM DisruptionEvent de2
+        de.EventDate <= '2025-12-31' and de.EventRecoveryDate >= '2019-01-01'
+    AND ('' = '' OR c.TierLevel = '')
 ) AS total
 WHERE
-    de.EventDate BETWEEN '2021-01-01' AND '2021-12-31'
+        de.EventDate <= '2025-12-31' and de.EventRecoveryDate >= '2019-01-01'
+    AND ('' = '' OR c.TierLevel = '')
 GROUP BY
     l.ContinentName,
     total.TotalEvents
 ORDER BY
     RRC DESC
+
+SELECT
+    l.CountryName,
+    COUNT(DISTINCT de.EventID) * 100.0 / total.TotalEvents AS RRC
+FROM DisruptionEvent de
+JOIN ImpactsCompany ic ON ic.EventID = de.EventID
+JOIN Company c ON ic.AffectedCompanyID = c.CompanyID
+JOIN Location l ON c.LocationID = l.LocationID
+CROSS JOIN (
+    SELECT COUNT(DISTINCT de2.EventID) AS TotalEvents
+    FROM DisruptionEvent de2
+    JOIN ImpactsCompany ic2 ON ic2.EventID = de2.EventID
+    JOIN Company c2 ON ic2.AffectedCompanyID = c2.CompanyID
+    WHERE de2.EventDate <= '2025-12-31'
+      AND de2.EventRecoveryDate >= '2019-01-01'
+) AS total
+WHERE de.EventDate <= '2025-12-31'
+  AND de.EventRecoveryDate >= '2019-01-01'
+AND ('2' = '' OR c.TierLevel = '2')-- Same Tier filter
+GROUP BY l.CountryName, total.TotalEvents
+ORDER BY RRC DESC
+
 
 
 --RRC v2 (separated month and year, cant get all months to output, unsure about selecting continent or country)
