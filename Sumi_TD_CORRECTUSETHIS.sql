f2122da01f60cb219477cc83f4ae07820f643d1c
sumnima-bhandari <bhanda21@purdue.edu>
2025-12-06 09:48:10 -0500

    Update Sumi_TD_CORRECTUSETHIS.sql

--- Sumi_TD_CORRECTUSETHIS.sql
+++ Sumi_TD_CORRECTUSETHIS.sql
@@ -22,6 +22,27 @@ FROM (
     GROUP BY e.EventId
 ) AS de;
 
+-- TD Histogram
+SELECT
+    e.EventId,
+    DATEDIFF(e.EventRecoveryDate, e.EventDate) AS RecoveryDays
+FROM DisruptionEvent AS e
+WHERE
+    e.EventDate <= '2025-12-31' AND e.EventRecoveryDate >= '2024-01-01'
+    AND EXISTS (
+        SELECT ic.EventID
+        FROM ImpactsCompany AS ic
+        JOIN Company  AS c ON c.CompanyId  = ic.AffectedCompanyID
+        JOIN Location AS l ON l.LocationId = c.LocationId
+        WHERE ic.EventId = e.EventId
+          AND ('Haynes-Long' = '' OR c.CompanyName   = 'Haynes-Long')
+          AND ('' = '' OR l.CountryName   = '')
+          AND ('' = '' OR l.ContinentName = '')
+          AND ('' = '' OR c.TierLevel     = '')
+    )
+GROUP BY e.EventId
+ORDER BY e.EventId ASC;
+
 --ART One Value
 SELECT
     COUNT(*) AS DisruptionCount,
