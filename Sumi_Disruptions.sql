88fe492b053699126d8ad13fa03c68d7fddb945a
sumnima-bhandari <bhanda21@purdue.edu>
2025-12-08 10:06:47 -0500

    Update Sumi_Disruptions.sql

--- Sumi_Disruptions.sql
+++ Sumi_Disruptions.sql
@@ -1,4 +1,4 @@
--- Disruption events for a company
+-- Disruption events by company
 SELECT
     e.EventId,
     dc.CategoryName
@@ -13,3 +13,18 @@ WHERE
     e.EventDate <= '2025-12-31' AND e.EventRecoveryDate >= '2019-01-01'
     AND ('Smith Inc' = '' OR c.CompanyName = 'Smith Inc')
 ORDER BY e.EventDate ASC, e.EventId;
+
+-- Disruption events by event type
+SELECT DISTINCT
+    c.CompanyName
+FROM DisruptionEvent AS e
+JOIN DisruptionCategory AS dc
+  ON dc.CategoryID = e.CategoryID
+JOIN ImpactsCompany AS ic
+  ON ic.EventId = e.EventId
+JOIN Company AS c
+  ON c.CompanyId = ic.AffectedCompanyID
+WHERE
+    e.EventDate <= '2025-12-31' AND e.EventRecoveryDate >= '2019-01-01'
+    AND ('' = '' OR dc.CategoryName = '')
+ORDER BY c.CompanyName;
