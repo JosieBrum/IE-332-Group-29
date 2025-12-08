dccf28bd6d221024a84da4c383b8294a30917daf
sumnima-bhandari <bhanda21@purdue.edu>
2025-12-08 15:32:29 -0500

    Update Sumi_Alerts.sql

--- Sumi_Alerts.sql
+++ Sumi_Alerts.sql
@@ -1,9 +1,12 @@
 SELECT
-    EventId,
-    EventDate,
-    EventRecoveryDate
-FROM DisruptionEvent
+    e.EventId,
+    e.EventDate,
+    e.EventRecoveryDate,
+    dc.CategoryName AS EventType
+FROM DisruptionEvent AS e
+JOIN DisruptionCategory AS dc
+    ON dc.CategoryID = e.CategoryID
 WHERE
-    EventRecoveryDate IS NULL
-    OR EventRecoveryDate > CURRENT_DATE
-ORDER BY EventDate ASC;
+    e.EventRecoveryDate IS NULL
+    OR e.EventRecoveryDate > CURRENT_DATE
+ORDER BY e.EventDate ASC;
