d8646d25e5b126b4de183079fa95113620a3db72
garza80 <garza80@purdue.edu>
2025-12-09 17:13:02 -0500

    Add files via upload

---
+++ AddCompany.sql
@@ -0,0 +1,35 @@
+-- Query 1: Get LocationID from City and Country
+SELECT LocationID 
+FROM Location 
+WHERE City = '' 
+  AND CountryName = ''
+  AND ContinentName = '';
+
+-- If LocationID does not exist, insert new location
+INSERT INTO Location (City, CountryName, ContinentName)
+VALUES ('', '', '');
+
+--Then User inputs Company Name
+
+-- Query 2: Check if Company Name Already Exists
+SELECT EXISTS(
+    SELECT 1 
+    FROM Company 
+    WHERE CompanyName = ''
+) AS CompanyExists;
+
+-- Query 3: Insert New Company
+INSERT INTO Company (CompanyName, TierLevel, Type, LocationID)
+VALUES ('', '', '', '');
+
+-- Query 4A: Insert into Manufacturer table
+INSERT INTO Manufacturer (CompanyID, FactoryCapacity)
+VALUES (LAST_INSERT_ID(), '');
+
+-- Query 4B: Insert into Distributor table
+INSERT INTO Distributor (CompanyID)
+VALUES (LAST_INSERT_ID());
+
+-- Query 4C: Insert into Retailer table
+INSERT INTO Retailer (CompanyID)
+VALUES (LAST_INSERT_ID());
